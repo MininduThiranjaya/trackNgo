e3a626dbd6402927194ee115de4b110071678c5e
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-10-30 04:32:50 +0530

    Update all_routes.js
    create route to get all buses

--- client_backend/all_routes.js
+++ client_backend/all_routes.js
@@ -3,12 +3,15 @@ const route = express.Router();
 const all_function = require('./all_function');
 
 // API to fetch all bus locations
+route.post('/search_for_buses', all_function.get_buses);
+
+// API to fetch all bus locations
 route.get('/get-bus-locations', all_function.get_bus_location);
 
 // Get location code by serching 
 route.post('/get-location-code-search-by-name', all_function.get_location_code_search_by_name);
 
 // Get route paths 
-route.post('/get-route', all_function.get_route);
+//route.post('/get-route', all_function.get_route);
 
 module.exports = route;
