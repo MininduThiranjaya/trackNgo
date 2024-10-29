56a5191517d9cff9dc8621cbd8ffc2f2e15ed525
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-10-30 04:33:51 +0530

    Create busCard.js
    create bus card

---
+++ frontend/src/com/busCard.js
@@ -0,0 +1,29 @@
+import Card from 'react-bootstrap/Card';
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+function BusCard({busRouteWithBus}) {
+  
+  return (
+    <Container className="my-3 p-4">
+      <Row xs={1} md={4} className='g-4 '>
+        {
+          busRouteWithBus.map((e) => {
+            return (
+              <Col>
+                <Card className="px-4" style={{ backgroundColor: '#f8f9fa' }}>
+                  <Card.Title>Card with Equal Left and Right Padding</Card.Title>
+                  <Card.Text>
+                    This card has equal padding on the left and right sides.
+                  </Card.Text>
+                </Card>
+              </Col>
+            )
+          })
+        }
+      </Row>
+    </Container>
+  );
+}
+
+export default BusCard;
