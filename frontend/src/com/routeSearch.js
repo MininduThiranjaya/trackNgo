2b905d8b05cdec8df529d2826bd3d1fcdaf6560b
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-10-30 04:34:17 +0530

    Create routeSearch.js

---
+++ frontend/src/com/routeSearch.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect} from 'react';
+import HeaderAndFooterExample from './routeSearchCard'
+
+export default function SearchRoute() {
+
+    return (
+        
+            <HeaderAndFooterExample/>
+            
+        
+    )
+}
