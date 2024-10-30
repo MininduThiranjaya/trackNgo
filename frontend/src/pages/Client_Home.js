9bde08ef40a938dbb29ebd1a5ecb03cbda60d789
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-10-30 18:58:42 +0530

    Create Client_Home.js
    create home page

---
+++ frontend/src/pages/Client_Home.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import SearchRoute from '../com/routeSearch';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/* The following line can be included in a src/App.scss */
+
+
+function Client_Home() {
+    return (
+        <>
+            <SearchRoute></SearchRoute>
+        </>
+    );
+}
+
+export default Client_Home;
