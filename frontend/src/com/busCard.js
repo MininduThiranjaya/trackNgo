c6fd850131dafdc5528823c6070f0de5b174e1e3
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-10-30 18:57:06 +0530

    Update busCard.js
    bus card

--- frontend/src/com/busCard.js
+++ frontend/src/com/busCard.js
@@ -1,5 +1,8 @@
 import Card from 'react-bootstrap/Card';
 import { Container, Row, Col } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationCrosshairs } from '@fortawesome/free-solid-svg-icons';
 
 
 function BusCard({busRouteWithBus}) {
@@ -12,10 +15,57 @@ function BusCard({busRouteWithBus}) {
             return (
               <Col key={index}>
                 <Card className="px-4" style={{ backgroundColor: '#f8f9fa' }}>
-                  <Card.Title>Card with Equal Left and Right Padding</Card.Title>
+                  <Card.Title>Route</Card.Title>
                   <Card.Text>
-                    This card has equal padding on the left and right sides.
+                    <p>
+                      <b>Starts : </b>{e.source}
+                    </p>
+                    <b>Stops : </b> 
+                    <ul>
+                        {
+                          e.stops.map((stop, index) => {
+                            return (
+                              <li key={index}>{stop}</li>
+                            )
+                          })
+                        }
+                    </ul>
+                    <b>Destination : </b>{e.destination}
+                    
+                    <p>
+                      <b>Buses :</b> 
+                    </p>
+                        {
+                          ((e.busName.length > 1)?
+                          (
+                            e.busName.map((e, index) => {
+                              return (
+                                <Container>
+                                  <Row>
+                                    <Col>{e}</Col>
+                                    <Col>Location</Col>
+                                    <Col>Status</Col>
+                                  </Row>
+                                </Container>
+                              )
+                            })
+                          ):(
+                            <Container>
+                              <Row>
+                                <Col>{e.busName}</Col>
+                                <Col>Location</Col>
+                                <Col>Status</Col>
+                              </Row>
+                            </Container>
+                          ))
+                        }
+
                   </Card.Text>
+                  <NavLink to={`/client-bus-route-map/${e._id}`} activeClassName="active">
+                    <FontAwesomeIcon icon={faLocationCrosshairs} size="lg"  className='px-1' style={{color: "#3512e2"}} />
+                    Find Me
+                  </NavLink>
+
                 </Card>
               </Col>
             )
