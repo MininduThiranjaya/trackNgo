5d44c34bb5906b6a1d62bb9c3d5ddf8f6cb15d55
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-10-30 18:56:12 +0530

    Update busRoute.js
    change route schema

--- client_backend/models/busRoute.js
+++ client_backend/models/busRoute.js
@@ -4,7 +4,7 @@ const routeSchema = new mongoose.Schema({
     source: String,
     destination: String,
     stops:[String],
-    busId:[{type:mongoose.Schema.Types.ObjectId, ref:'Bus'}],
+    busName:[String],
     lastUpdated: { type: Date, default: Date.now },
 });
 
