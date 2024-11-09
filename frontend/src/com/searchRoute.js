4e943ac344e556fdcce88e68f7fd62fa9744dbc0
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-11-09 12:47:57 +0530

    Create searchRoute.js

---
+++ frontend/src/com/searchRoute.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect} from 'react';
+import SearchRouteCard from './searchRouteCard'
+
+export default function SearchRoute() {
+
+    return (  
+            <SearchRouteCard/>
+    )
+}
