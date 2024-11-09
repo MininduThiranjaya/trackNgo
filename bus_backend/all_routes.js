1bf494b647fff2e07ac5335002fd5fad4844611b
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-11-09 22:25:23 +0530

    Update all_routes.js

--- bus_backend/all_routes.js
+++ bus_backend/all_routes.js
@@ -3,6 +3,6 @@ const route = express.Router();
 const all_function = require('./all_function');
 
 // API to update bus location
-route.post('/update-location', all_function.update_location);
+route.post('/update-location/', all_function.update_location);
 
 module.exports = route;
