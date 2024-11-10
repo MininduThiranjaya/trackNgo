c4b9f5090d4fb0903d6ce443c56303fba1a7e3ea
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-11-11 01:10:25 +0530

    Update Client_Bus_Route_Map.js

--- frontend/src/pages/Client_Bus_Route_Map.js
+++ frontend/src/pages/Client_Bus_Route_Map.js
@@ -1,20 +1,19 @@
 // src/App.js
 import React from 'react';
-import SearchRoute from '../com/searchRoute';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useParams } from 'react-router-dom';
-import ClientMap from '../com/clientMap';
-import Error from './Error';
 import { NavLink } from 'react-bootstrap';
 
-/* The following line can be included in a src/App.scss */
-
+import ClientMap from '../com/clientMap'; // Client Map component
+import Error from './Error'; // Error page
 
 export default function Client_Bus_Route_Map() {
     
-    const { routeId, busId } = useParams();
+    const { routeId, busId } = useParams(); // extract routeId and BusId from URL
 
     return (
+
+        //check the route or bus id is missing or not, if not navigae to error page
         <>
             {routeId && busId ? (
                 <ClientMap routeId={routeId} busId={busId}/>
