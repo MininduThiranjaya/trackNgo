fcaf7569281405ea46c36001375dd36055147c36
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-11-11 01:10:29 +0530

    Update Client_Home.js

--- frontend/src/pages/Client_Home.js
+++ frontend/src/pages/Client_Home.js
@@ -1,10 +1,8 @@
 // src/App.js
 import React from 'react';
-import SearchRoute from '../com/searchRoute';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-/* The following line can be included in a src/App.scss */
-
+import SearchRoute from '../com/searchRoute'; // Search route component
 
 function Client_Home() {
     return (
