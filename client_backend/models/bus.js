0a0c3aadbf0610019c386796ce6cb73690c5c64c
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-11-12 10:22:05 +0530

    Update bus.js

--- client_backend/models/bus.js
+++ client_backend/models/bus.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const BusSchema = new mongoose.Schema({
     busNameId: String,
+    isActive: Boolean,
     latitude: Number,
     longitude: Number,
     lastUpdated: { type: Date, default: Date.now },
