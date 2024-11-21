4aeabd3ff6676551145ab0950cbd0f8fafbfd222
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-11-21 13:35:46 +0530

    Update bus.js

--- client_backend/models/bus.js
+++ client_backend/models/bus.js
@@ -1,12 +1,47 @@
 const mongoose = require('mongoose');
 
-const BusSchema = new mongoose.Schema({
-    busNameId: String,
-    isActive: Boolean,
-    latitude: Number,
-    longitude: Number,
-    lastUpdated: { type: Date, default: Date.now },
-});
+const BusSchema = new mongoose.Schema(
+    {
+
+        busNumber: {
+            type: String,
+            required: true
+        },
+        startLocation: {
+            type: String,
+            required: true
+        },
+        endLocation: {
+            type: String,
+            required: true
+        },
+        routeNumber: {
+            type: String,
+            required: true
+        },
+        fareEstimate: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            enum: ['Normal', 'Semi-Luxury', 'Luxury'], default: 'Normal'
+        },
+        status: {
+            type: Boolean,
+            default: false
+        },
+        latitude: {
+            type: Number,
+        },
+        longitude: {
+            type: Number,
+        }
+    },
+    {
+        timestamps: true,
+    },
+);
 
 const Bus = mongoose.model('buses', BusSchema);
 
