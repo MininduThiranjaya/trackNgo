a12923a53f8ff5a18606f28b51ff0c2b10876ea3
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-11-21 14:37:15 +0530

    update

--- bus_backend/src/models/busLocation.js
+++ bus_backend/src/models/busLocation.js
@@ -2,10 +2,22 @@ const mongoose = require('mongoose');
 
 const BusLocationSchema = new mongoose.Schema({
 
-    busNumber: {type : String, required: true},
-    latitude: {type : Number , default: 0},
-    longitude: {type : Number, default: 0},
-    lastUpdated: { type: Date, default: Date.now },
+    busNumber: {
+        type : String,
+        required: true
+    },
+    latitude: {
+        type : Number,
+        default: 0
+    },
+    longitude: {
+        type : Number,
+        default: 0
+    },
+    lastUpdated: {
+        type: Date,
+        default: Date.now
+    }
 }); 
 
 
