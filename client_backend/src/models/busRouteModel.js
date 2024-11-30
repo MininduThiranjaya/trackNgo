fe94f28fe41e95d68e304381c2a7d6aff6a8d464
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-11-30 22:15:30 +0530

    Update busRouteModel.js
    bus route model changes

--- client_backend/src/models/busRouteModel.js
+++ client_backend/src/models/busRouteModel.js
@@ -23,10 +23,10 @@ const busRouteSchema = new mongoose.Schema(
       type: [String],
       default: [],
     },
-    status: {
-      type: [Boolean],
-      default: false,
-    },
+    // status: {
+    //   type: [Boolean],
+    //   default: false,
+    // },
   },
   { timestamps: true }
 )
